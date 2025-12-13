a761699954375edda6ce47a6eb6fb70ae0d29198
nada <nadaaabdlla@gmail.com>
2025-12-14 01:57:38 +0200

    .

--- main.js
+++ main.js
@@ -1,22 +1,72 @@
 let cards = document.querySelectorAll("div");
 let imgs = document.querySelectorAll("img");
-let c=[];
+let start = document.querySelector("#start");
+let startCon = document.querySelector("#startCon");
+let rightCounterP = document.querySelector("#rightCounter");
+let wrongCounterP = document.querySelector("#wrongCounter");
+let timer = document.querySelector("#timer");
+let compareName = [];
+let openedCard = [];
+let rightCounter = 0;
+let wrongCounter = 0;
+let timerSec = 0;
+document.addEventListener("DOMContentLoaded", () => {
     cards.forEach((card) => {
-         card.addEventListener("click", (e) => {
-            e.currentTarget.classList.add("transform");
-            e.currentTarget.querySelector("img").classList.add("clicked");
-            e.currentTarget.classList.remove("back");
-            e.currentTarget.querySelector("img").classList.remove("imgDisappear");
-            c.push(e.currentTarget.querySelector("img").alt);
-              if (c[0]!=c[1]) {
-                    e.currentTarget.classList.remove("transform");
-                    e.currentTarget.querySelector("img").classList.remove("clicked");
-                    e.currentTarget.classList.add("back");
-                    e.currentTarget.querySelector("img").classList.add("imgDisappear");
-                };
-                if (c.length==2) {
-                    c=[];
-                };
-                console.log(c);
-        });
+        card.style.order = `${Math.floor(Math.random() * 240)}`;
     });
+});
+start.addEventListener("click", () => {
+    startCon.style.display = "none";
+    let timeCounter = setInterval(() => { timer.innerHTML = `<span>Time:</span> ${timerSec++} second`; }, 1000);
+    setInterval(() => {
+        if ([...cards].every(card => card.querySelector("img").classList.contains("clicked"))) {
+            clearInterval(timeCounter);
+            
+        };
+    }, 1000);
+    setTimeout(() => {
+        cards.forEach((card) => {
+            card.classList.remove("back");
+            card.querySelector("img").classList.remove("imgDisappear");
+        });
+    }, 0);
+    setTimeout(() => {
+        cards.forEach((card) => {
+            card.classList.add("back");
+            card.querySelector("img").classList.add("imgDisappear");
+        });
+    }, 5000);
+});
+cards.forEach((card) => {
+    card.addEventListener("click", (e) => {
+        const currentCard = e.currentTarget;
+        const img = currentCard.querySelector("img");
+        currentCard.classList.add("transform");
+        currentCard.classList.remove("back");
+        img.classList.add("clicked");
+        img.classList.remove("imgDisappear");
+        compareName.push(img.alt);
+        openedCard.push(currentCard);
+        if (compareName.length === 2) {
+            if (compareName[0] !== compareName[1]) {
+                setTimeout(() => {
+                    openedCard.forEach((e) => {
+                        e.classList.remove("transform");
+                        e.classList.add("back");
+                        e.querySelector("img").classList.remove("clicked");
+                        e.querySelector("img").classList.add("imgDisappear");
+                    });
+                    compareName = [];
+                    openedCard = [];
+                }, 1000);
+                wrongCounter++;
+                wrongCounterP.innerHTML = `<span>Wrong Answers:</span> ${wrongCounter}`;
+            } else {
+                compareName = [];
+                openedCard = [];
+                rightCounter++;
+                rightCounterP.innerHTML = `<span>Right Answers:</span> ${rightCounter}`;
+            };
+        }
+    });
+});
